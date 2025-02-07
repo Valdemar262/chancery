852d47f79a7a2bc927eea6f1fa308feda229be5a
Vladimir <bubnovv661@gmail.com>
2025-02-07 17:25:39 +0300

    CHANCERY_API-4. Контрольный рефакторинг

--- app/routes/api.php
+++ app/routes/api.php
@@ -10,27 +10,28 @@ Route::post('/refresh', [AuthController::class, 'refresh']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:api']], function () {
+    Route::group(['middleware' => ['role:client']], function () {
 
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+
+        Route::get('/allUsers', [UserController::class, 'getAllUsers']);
+        Route::get('/showUser/{user}', [UserController::class, 'showUser']);
+        Route::put('/updateUser', [UserController::class, 'updateUser']);
+
+        Route::get('/allStatements', [StatementController::class, 'getAllStatements']);
+        Route::get('/showStatement/{statement}', [StatementController::class, 'showStatement']);
+        Route::post('/createStatement', [StatementController::class, 'createStatement']);
+        Route::put('/updateStatement', [StatementController::class, 'updateStatement']);
+        Route::delete('/deleteStatement/{id}', [StatementController::class, 'deleteStatement']);
+    });
     Route::group(['middleware' => ['role:admin']], function () {
+
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
 
         Route::post('assignRole/{user}', [UserController::class, 'assignRole']);
         Route::post('removeRole/{user}', [UserController::class, 'removeRole']);
         Route::delete('/deleteUser/{id}', [UserController::class, 'deleteUser']);
-
-        Route::group(['middleware' => ['role:client']], function () {
-
-            Route::post('/logout', [AuthController::class, 'logout']);
-            Route::get('/me', [AuthController::class, 'me']);
-
-            Route::get('/allUsers', [UserController::class, 'getAllUsers']);
-            Route::get('/showUser/{user}', [UserController::class, 'showUser']);
-            Route::put('/updateUser', [UserController::class, 'updateUser']);
-
-            Route::get('/allStatements', [StatementController::class, 'getAllStatements']);
-            Route::get('/showStatement/{statement}', [StatementController::class, 'showStatement']);
-            Route::post('/createStatement', [StatementController::class, 'createStatement']);
-            Route::put('/updateStatement', [StatementController::class, 'updateStatement']);
-            Route::delete('/deleteStatement/{id}', [StatementController::class, 'deleteStatement']);
-        });
     });
 });
